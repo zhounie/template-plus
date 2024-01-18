4744d7733466ff88b74d248dcda3dd8cb8763688
zn <zn@pamocloud.com>
2024-01-18 17:30:01 +0800

    add version command.

--- packages/templates/index.js
+++ packages/templates/index.js
@@ -2,6 +2,11 @@
 
 import { program } from 'commander'
 import init from './dist/index.mjs'
+import pck from './package.json' assert { type: 'json'}
+
+program
+    .version(pck.version)
+
 
 program
     .command('init')
