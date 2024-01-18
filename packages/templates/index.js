bb1dc9d5f0c08ac05a71a4939b8c33abe7cc3244
zn <zn@pamocloud.com>
2024-01-18 17:58:57 +0800

    change path.

--- packages/templates/index.js
+++ packages/templates/index.js
@@ -4,7 +4,7 @@ import { program } from 'commander'
 import fs from 'node:fs'
 import path from 'node:path'
 
-const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), 'utf-8'))
+const pkg = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '../package.json'), 'utf-8'))
 
 program
     .version(pkg.version)
